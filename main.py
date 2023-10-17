4d75911c02ed563b301001682efc567fe06a7e97
Adam Grzymkowski <adam.grzymkowski147@gmail.com>
2023-10-17 23:27:33 +0200

    Finall Version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import tmdb_client
 import random
 
@@ -6,9 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_popular_movies()["results"][:12]
+    selected_list = request.args.get('list_type', 'popular')
+    available_lists = tmdb_client.get_available_lists()
+    if selected_list not in available_lists:
+        selected_list = 'popular'
+    movies = tmdb_client.get_movies_list(list_type=selected_list)["results"][:12]
     random.shuffle(movies)
-    return render_template("homepage.html", movies=movies)
+    return render_template("homepage.html", movies=movies, current_list=selected_list, available_lists=available_lists)
 
 @app.context_processor
 def utility_processor():
@@ -19,7 +23,10 @@ def utility_processor():
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
     details = tmdb_client.get_single_movie(movie_id)
-    return render_template("movie_details.html", movie = details)
+    cast = tmdb_client.get_single_movie_cast(movie_id)
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])
+    return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -18,6 +18,10 @@ def get_poster_url(poster_path, size = "w342"):
     base_url = "https://image.tmdb.org/t/p"
     return f"{base_url}/{size}/{poster_path}"
 
+def get_bcakdrop_url(backdrop_path, size = "w780"):
+    base_url = "https://image.tmdb.org/t/p"
+    return f"{base_url}/{size}/{backdrop_path}"
+
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
     headers = {
@@ -26,3 +30,36 @@ def get_single_movie(movie_id):
     response = requests.get(endpoint, headers=headers)
     return response.json()
 
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_movies_list(list_type="popular"):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+def get_available_lists():
+    available_lists = {
+        'popular': 'Popular',
+        'top_rated': 'Top Rated',
+        'upcoming': 'Upcoming',
+        'now_playing': 'Now Playing'
+    }
+    return available_lists
